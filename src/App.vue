a83b75b69e22ff9d7fc719d2bbadc40825fc04c6
Washimbar <washimbari0001@gmail.com>
2023-08-22 20:32:55 +0530

    first

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,18 @@
 <template>
   <div>
-    <h1>hello</h1>
+    <h1>Hello {{ name }}</h1>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'App',
-    setup(){
-      return {
-        name: 'Washim Bari',
-      }
+export default {
+  name: 'App',
+  setup() {
+    return {
+      name: 'Washim Bari',
     }
   }
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
